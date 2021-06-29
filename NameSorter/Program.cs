30897c7f4f7935006499f7fb9081fb9df3f22560
susmitha <49911847+nsusmitha@users.noreply.github.com>
2021-06-29 11:15:15 +1000

    Add project files.

---
+++ NameSorter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Configuration;
+
+
+namespace NameSorter
+{
+    class Program
+    {
+        private static string GetLastWordToFirst(string word)
+        {
+           
+            var words = word.Split(' ');
+            
+            var length = words.Length;
+
+            return words[length - 1] + " " + string.Join(" ", words, 0, length - 1);
+            
+
+        }
+        static void Main(string[] args)
+        {
+
+            string[] lines = File.ReadAllLines(@"D:\unsorted-names-list.txt");
+
+
+            var sortstring = lines.Select(GetLastWordToFirst).ToArray();
+
+
+            System.Array.Sort(sortstring, lines);
+
+          
+            
+
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            File.WriteAllLines(@"D:\sorted-names-list.txt", lines);
+
+
+            Console.ReadLine();
+        }
+    }
+
+}
