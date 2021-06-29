cbe69cc7027a49515fe083ae9513de7d52c55aa0
susmitha <49911847+nsusmitha@users.noreply.github.com>
2021-06-30 00:58:10 +1000

    given text file path in solution explorer

--- NameSorter/Program.cs
+++ NameSorter/Program.cs
@@ -1,48 +1,62 @@
-﻿using System;
+﻿
+using System;
 using System.IO;
 using System.Linq;
 using System.Configuration;
-
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace NameSorter
 {
     class Program
     {
-        private static string GetLastWordToFirst(string word)
-        {
-           
-            var words = word.Split(' ');
-            
-            var length = words.Length;
 
-            return words[length - 1] + " " + string.Join(" ", words, 0, length - 1);
-            
+        //public static IConfiguration Configuration;
 
-        }
+
         static void Main(string[] args)
         {
-
-            string[] lines = File.ReadAllLines(@"D:\unsorted-names-list.txt");
-
-
-            var sortstring = lines.Select(GetLastWordToFirst).ToArray();
-
-
-            System.Array.Sort(sortstring, lines);
-
-          
-            
-
+            //ConfigureServices();
+            //string unSortedFilePath = Configuration["UnsortPaths:Upath"];
+            //string sortedFilePath = Configuration["sortPaths:SPath"];
+                        
+              string []   lines = File.ReadAllLines("unsorted-names-list.txt");
+                var sortstring = lines.Select(GetLastWordToFirst).ToArray();
+                Array.Sort(sortstring, lines);
 
             foreach (string line in lines)
             {
                 Console.WriteLine(line);
             }
-            File.WriteAllLines(@"D:\sorted-names-list.txt", lines);
-
+          
+            File.WriteAllLines(@"sorted-names-list.txt", lines);
 
             Console.ReadLine();
         }
+
+        private static string GetLastWordToFirst(string word)
+        {
+
+            var words = word.Split(' ');
+
+            var length = words.Length;
+
+            return words[length - 1] + " " + string.Join(" ", words, 0, length - 1);
+
+
+        }
+
+        //private static void ConfigureServices()
+        //{
+
+        //    // Build configuration
+        //    Configuration = new ConfigurationBuilder()
+        //            .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+        //            .AddJsonFile("config.json", false)
+        //            .Build();
+
+        //}
     }
 
 }
+
